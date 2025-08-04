61eed8084a870181234c8e55b004b64a5ae7e945
lpcarvalho97 <lucasadmcarvalho@outlook.com>
2025-08-04 08:40:19 -0300

    Incluídos todos campos view 1

--- laravel/resources/views/consultas/index.blade.php
+++ laravel/resources/views/consultas/index.blade.php
@@ -18,20 +18,38 @@
         <table id="tabelaAghu">
             <thead>
                 <tr>
-                    <th>Nome</th>
+                    <th>Número da Consulta</th>
+                    <th>Data Marcação</th>
                     <th>Data Consulta</th>
-                    <th>Unidade</th>
+                    <th>Dia da Semana</th>
+                    <th>Número do Prontuário</th>
+                    <th>Grade</th>
+                    <th>Nome</th>
+                    <th>Unidade Funcional</th>
                     <th>Especialidade</th>
+                    <th>Equipe</th>
+                    <th>Nome do Profisional</th>
+                    <th>Telefone para Recado</th>
+                    <th>Telefone para Contato</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($consultasAghu as $consulta)
                 
                     <tr>
-                        <td>{{ $consulta->nome_paciente }}</td>
+                        <td>{{ $consulta->nr_consulta }}</td>
+                        <td>{{ $consulta->data_marcacao }}</td>
                         <td>{{ $consulta->data_consulta }}</td>
+                        <td>{{ $consulta->dia_semana }}</td>
+                        <td>{{ $consulta->nr_prontuario }}</td>
+                        <td>{{ $consulta->grade }}</td>
+                        <td>{{ $consulta->nome_paciente }}</td>
                         <td>{{ $consulta->unidade_funcional }}</td>
                         <td>{{ $consulta->especialidade }}</td>
+                        <td>{{ $consulta->equipe }}</td>
+                        <td>{{ $consulta->nome_profissional}}</td>
+                        <td>{{ $consulta->fone_recado}}</td>
+                        <td>{{ $consulta->fone_contato}}</td>
                     </tr>
                 @endforeach
             </tbody>
@@ -42,20 +60,76 @@
         <table id="tabelaSisfam">
             <thead>
                 <tr>
+                    <th>APAC</th>
+                    <th>Status Financeiro</th>
+                    <th>Autorizado</th>
+                    <th>Profissional Solicitante</th>
+                    <th>Prontuário</th>
                     <th>Paciente</th>
+                    <th>CID</th>
+                    <th>Descrição do Procedimento</th>
+                    <th>Código do Procedimento</th>
+                    <th>Nome do Procedimento</th>
+                    <th>Tipo APAC</th>
+                    <th>Situação</th>
+                    <th>Motivo Perm Saída</th>
+                    <th>Solicitação</th>
+                    <th>Criação</th>
+                    <th>Atualização</th>
+                    <th>Validade</th>
+                    <th>Motivo Saída</th>
+                    <th>Localidade</th>
+                    <th>Sol Aut</th>
+                    <th>Situação APAC</th>
+                    <th>Cod Tabela string</th>
+                    <th>Descrição do Grupo</th>
+                    <th>Meta Mensal</th>
+                    <th>Multiplicador</th>
+                    <th>Área Especialidade</th>
                     <th>Data Cirurgia</th>
-                    <th>Procedimento</th>
-                    <th>Status</th>
+                    <th>Cirurgia < 18 meses</th>
+                    <th>Atendido por Dr. Marcelo</th>
+                    <th>Processo Associado</th>
+                    <th>APAC Emitida + 3 meses</th>
+                    <th>Total de registros</th>
                 </tr>
             </thead>
 
             <tbody>
                 @foreach ($consultasSisfam as $consulta)
                 <tr>
+                    <td>{{ $consulta->id_apac }}</td>
+                    <td>{{ $consulta->status_financeiro }}</td>
+                    <td>{{ $consulta->autorizado }}</td>
+                    <td>{{ $consulta->profissional_solicitante }}</td>
+                    <td>{{ $consulta->prontuario }}</td>
                     <td>{{ $consulta->paciente }}</td>
-                    <td>{{ $consulta->data_cirurgia }}</td>
+                    <td>{{ $consulta->cid }}</td>
+                    <td>{{ $consulta->procedimento_descricao }}</td>
+                    <td>{{ $consulta->codigo_procedimento }}</td>
                     <td>{{ $consulta->nome_procedimento }}</td>
+                    <td>{{ $consulta->tipo_apac }}</td>
                     <td>{{ $consulta->situacao }}</td>
+                    <td>{{ $consulta->motivo_perm_saida }}</td>
+                    <td>{{ $consulta->solicitacao }}</td>
+                    <td>{{ $consulta->criacao }}</td>
+                    <td>{{ $consulta->atualizacao }}</td>
+                    <td>{{ $consulta->validade }}</td>
+                    <td>{{ $consulta->motivo_saida }}</td>
+                    <td>{{ $consulta->localidade }}</td>
+                    <td>{{ $consulta->sol_aut }}</td>
+                    <td>{{ $consulta->situacao_apac }}</td>
+                    <td>{{ $consulta->cod_tabela_string }}</td>
+                    <td>{{ $consulta->descricao_grupo }}</td>
+                    <td>{{ $consulta->meta_mensal }}</td>
+                    <td>{{ $consulta->multiplicador }}</td>
+                    <td>{{ $consulta->area_especialidade }}</td>
+                    <td>{{ $consulta->data_cirurgia }}</td>
+                    <td>{{ $consulta->cirurgia_menor_18_meses }}</td>
+                    <td>{{ $consulta->atendido_dr_marcelo }}</td>
+                    <td>{{ $consulta->proc_associado }}</td>
+                    <td>{{ $consulta->apac_emitida_mais_3_meses }}</td>
+                    <td>{{ $consulta->total_registros }}</td>
                 </tr>
                 @endforeach
         </table>
@@ -74,7 +148,7 @@
             }
 
             aplicarFiltro('filtroAghu', 'tabelaAghu', 0); //Filtra os dados do AGHU por nome de paciente
-            aplicarFIltro('filtroSisfam', 'tabelaSisfam', 0); //FIltra os dados do Sisfam por nome de paciente
+            aplicarFiltro('filtroSisfam', 'tabelaSisfam', 0); //FIltra os dados do Sisfam por nome de paciente
         </script>
 
 
